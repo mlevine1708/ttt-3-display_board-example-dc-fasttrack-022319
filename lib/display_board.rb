de7cb74f1d329030b1c39b2dadeaafaff411a6b8
Micah Levine <micah.levine@outlook.com>
2019-02-24 17:44:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+// ♥ irb
+001:0 > def display_board
+002:1 >     puts "A Tic Tac Toe Board"
+003:1 >   end
